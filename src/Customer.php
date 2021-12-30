a3c8d3fe76a23df24144f0cf6a72e7ecc08caee2
gannets <gannetnadi@gmail.com>
2021-12-30 10:30:56 +0700

    update
    jika tidak ada paramenter di function hallo

--- src/Customer.php
+++ src/Customer.php
@@ -6,7 +6,7 @@ class Customer {
 
     public function __construct(private string $name){}
 
-    public function sayHello(string $name){
+    public function sayHello(string $name = "Guest"){
         return " Hallo $name, my name is $this->name";
     }
 }
